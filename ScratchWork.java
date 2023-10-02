7231020bb60633e96c4e3fe1865687e13a4f3043
Kelly <1lckelly@seattleschools.org>
2023-10-02 09:25:16 -0700

    Initial Commit of file
    Demonstrated GIT in front of class and wated to show off a good commit message
    
    This file is not part of any assignment, only for demo purposes

---
+++ ScratchWork.java
@@ -0,0 +1,9 @@
+public class ScratchWork {
+   public static void main(String[] args) {
+System.out.println("Now, this is a story all about how");
+System.out.println("My life got flipped-turned upside down");
+System.out.println("And I'd like to take a minute");
+System.out.println("Just sit right there");
+System.out.println("I'll tell you how I became the prince of a town called Bel-Air");
+   }
+}
