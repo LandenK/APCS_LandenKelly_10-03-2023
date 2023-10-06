46629d3082404196737ba75dffb2abab91e11e6d
Kelly <1lckelly@seattleschools.org>
2023-10-06 09:03:53 -0700

    This is an assignment that runs five Clock functions

---
+++ Clock.java
@@ -0,0 +1,44 @@
+/*
+Landen K, Period 1, AP Computer Science, Assignment: Create your own Object Class
+*/
+
+// The method that calls the first dialogue code.
+public class Clock {
+   public static void main(String[] args) {
+   
+   // The method that calls the tellTheTime function.
+   tellTheTime();
+   // The method that calls the setTheTime function.
+   setTheTime();
+   // The method that calls the setTimer function.
+   setTimer();
+   // The method that calls the stopTimer function.
+   stopTimer();
+   // The method that calls the currentMinutes function.
+   currentMinutes();  
+      } 
+   // First method
+   public static void tellTheTime() {
+         
+      }
+   
+   // Second method
+   public static void setTheTime() {
+
+      }
+   
+   // Second method
+   public static void setTimer() {
+   
+      }
+   
+   // Second method
+   public static void stopTimer() {
+   
+      }
+   
+   // Second method
+   public static void currentMinutes() {
+      
+   }
+}
