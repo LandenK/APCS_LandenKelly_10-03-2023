813ab3d3369e37fd2f575f1c1aa0ca9cdb4c8247
Kelly <1lckelly@seattleschools.org>
2023-10-17 09:26:06 -0700

    This prints out my lifespan in days and days until the AP exam.

--- InterestingDays.java
+++ InterestingDays.java
@@ -1,16 +1,9 @@
-import java.util.Date;
-import java.time.LocalDate;
-
 public class InterestingDays {
    public static void main(String[] args) {
-      int thisYear;
-      int thisDay;
-      Day todayDay = new Day();
       Day apDay = new Day(2023, 5, 8);
       Day birthDay = new Day(2006, 2, 17);
       Day toDay = new Day(2023, 10, 17);
       
-      thisYear = todayDay.getYear();
       System.out.print("Landen is ");
       System.out.print(toDay.daysFrom(birthDay));
       System.out.println(" days old.");
