93f124e3907238d917f5076af5c669de733ac4ee
Kelly <1lckelly@seattleschools.org>
2023-10-19 09:20:21 -0700

    This is code that lets you choose a picture, print the picture as well as its height and width.

--- PictureExercise.java
+++ PictureExercise.java
@@ -1,22 +1,29 @@
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
-public class PictureExercise {
-
- public static void main(String[] args) {
-  try {
-   File f = new File("scenery.jpg");
-   BufferedImage image = ImageIO.read(f);
-   int height = image.getHeight();
-   int width = image.getWidth();
-   System.out.println("Height : " + height);
-   System.out.println("Width : " + width);
-  } catch (IOException ioe) {
-   ioe.printStackTrace();
-  }
- }
+public class PictureExercise
+{
+    public static void main(String[] args)
+    {    	
+        // If you want to not have to pick the image each time, fill in the correct address in the next line and delete the "//" 
+        // Picture clouds = new Picture("C:\\Pictures\\...");
+        Picture myPicture = new Picture();
+        myPicture.pick();
+        
+        // Use Picture methods to draw your picture object HERE
+        
+        // Draws the picture
+        myPicture.draw();
+        
+        // Gets the height
+        int height = myPicture.getHeight();
+        int width = myPicture.getWidth();
+        
+        // Use Picture methods & print\println to print out the height and width of your picture HERE
+        
+        // Prints the height
+        System.out.println("Height : " + height);
+        System.out.println("Width : " + width);
+        
+    }
 }
 
-// int name = function ----> print the function using the name
+// int name = function -> print the function using the name
